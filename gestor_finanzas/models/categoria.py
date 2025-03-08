cd3288561f122ba9ccb5e248c2c5a28a8ceeb9c8
ocamilavillero09 <111584990+ocamilavillero09@users.noreply.github.com>
2025-03-08 14:14:53 -0500

    Delete gestor_finanzas directory

--- gestor_finanzas/models/categoria.py
+++
@@ -1,4 +0,0 @@
-class Categoria:
-    def __init__(self, id_categoria: int, nombre: str):
-        self.id_categoria = id_categoria
-        self.nombre = nombre
--- gestor_finanzas/models/usuario.py
+++
@@ -1,6 +0,0 @@
-class Usuario:
-    def __init__(self, id_usuario: int, nombre: str, contraseña: str, email: str):
-        self.id_usuario = id_usuario
-        self.nombre = nombre
-        self.contraseña = contraseña
-        self.email = email
