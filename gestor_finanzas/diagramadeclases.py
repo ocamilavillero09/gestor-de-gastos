48eaf8ea7945cc83380cb1cdbad4d5e4556bab07
ocamilavillero09 <111584990+ocamilavillero09@users.noreply.github.com>
2025-03-29 10:51:15 -0500

    Delete gestor_finanzas directory

--- gestor_finanzas/diagramadeclases.py
+++
@@ -1,6 +0,0 @@
-""" diagrama de clases
-https://lucid.app/lucidchart/8f5eefd7-0763-48af-8c22-a7fd497cc3eb/edit?viewport_loc=-701%2C-466%2C2504%2C1249%2C0_0&invitationId=inv_e5db9645-9a7f-489c-b8dc-4199204ff153
-
-Word con explicacion de casos de prueba
-https://1drv.ms/w/c/dfcc97c591df8981/ESPdSUpCO7lDhAOyh9N4bHsBUlkCj-fTGth5wXgBEYdb4g?e=7MDMwz
-"""
